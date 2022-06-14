4196070a2df9d7b2e0a81836fbb7a84442b83f42
Rodrigo Soares <rddoisgo@ufu.br>
2022-06-14 11:35:36 -0300

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Rodrigo Soares on 11/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
