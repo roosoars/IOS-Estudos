4b61615b83dfbf2089de5d28db3602aca4eae558
Rodrigo Soares <rddoisgo@ufu.br>
2022-06-14 17:17:39 -0300

    Add files via upload

---
+++ I am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Rodrigo Soares on 14/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
